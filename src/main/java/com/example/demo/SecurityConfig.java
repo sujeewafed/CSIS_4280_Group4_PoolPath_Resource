447c058fb94f3aa221d47a879b70793bfb526553
Sujeewa Fernando <fernandow3@student.douglascollege.ca>
2025-04-05 16:22:11 -0700

    Resource project initial commit

---
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class SecurityConfig {
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+		return http.authorizeHttpRequests(
+
+				authorize -> authorize.requestMatchers(HttpMethod.GET, "/api/poolpath/teams/**")
+						.hasAuthority("SCOPE_poolpath:read").requestMatchers(HttpMethod.POST, "/api/poolpath/teams/**")
+						.hasAuthority("SCOPE_poolpath:admin").anyRequest().authenticated()
+
+		).oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())).build();
+
+	}
+}
---
+++ src/main/java/com/example/demo/controller/RideController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/poolpath")
+@RestController
+public class RideController {
+
+	@GetMapping("/rides")
+	public List<String> getRides() {
+		return List.of("Ride1", "Ride2", "Ride3");
+	}
+	
+	@PostMapping("/rides")
+	public String addRide(@RequestBody String rideName) {
+		return rideName + " added";
+	}
+}
