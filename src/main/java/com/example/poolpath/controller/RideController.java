2d95415d746b5172d2ddc227c5f982254adb3718
Gayashan <gayazhanmit@yahoo.com>
2025-04-07 13:15:24 -0700

    build errors fixed

--- src/main/java/com/example/poolpath/controller/RideController.java
+++ src/main/java/com/example/poolpath/controller/RideController.java
@@ -2,23 +2,23 @@ package com.example.poolpath.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/poolpath")
-@RestController
+//@RequestMapping("/api/poolpath")
+//@RestController
 public class RideController {
 
-	@GetMapping("/rides")
-	public List<String> getRides() {
-		return List.of("Ride1", "Ride2", "Ride3");
-	}
-	
-	@PostMapping("/rides")
-	public String addRide(@RequestBody String rideName) {
-		return rideName + " added";
-	}
+//	@GetMapping("/rides")
+//	public List<String> getRides() {
+//		return List.of("Ride1", "Ride2", "Ride3");
+//	}
+//	
+//	@PostMapping("/rides")
+//	public String addRide(@RequestBody String rideName) {
+//		return rideName + " added";
+//	}
 }
