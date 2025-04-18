2143d16fa9c638939fb895291d358a0feb58af0d
Sujeewa Fernando <fernandow3@student.douglascollege.ca>
2025-04-18 14:46:07 -0700

    uncommented the Auth settings

--- src/main/java/com/example/poolpath/SecurityConfig.java
+++ src/main/java/com/example/poolpath/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.example.poolpath;
 
-/*
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,10 +8,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-*/
+
 // @Configuration
 public class SecurityConfig {
-	/*
+	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
@@ -24,5 +24,5 @@ public class SecurityConfig {
 		).oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())).build();
 
 	}
-	*/
+	
 }
