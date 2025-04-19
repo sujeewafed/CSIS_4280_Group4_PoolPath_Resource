5d86a45dbe2d02023c9a4582927f0e0e05486c95
Indika <gayazhanmit@yahoo.com>
2025-04-19 03:48:18 -0700

    added swagger ui

--- src/main/java/com/example/poolpath/controller/UserController.java
+++ src/main/java/com/example/poolpath/controller/UserController.java
@@ -27,20 +27,20 @@ public class UserController {
 
 	@Autowired
 	UserRepository userRepository;
-		
+
 	// To get all users
 	@GetMapping("/users")
 	public ResponseEntity<List<User>> getAllUsers() {
 		try {
 			List<User> users = new ArrayList<User>();
-			userRepository.findAll().forEach(users::add); 
+			userRepository.findAll().forEach(users::add);
 			return new ResponseEntity<>(users, HttpStatus.OK);
 
 		} catch (Exception e) {
 			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-	
+
 	// To get an user by username
 	@GetMapping("/users/by_username")
 	public ResponseEntity<User> getUserByUsername(@PathVariable("username") String username) {
@@ -51,7 +51,7 @@ public class UserController {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 	}
-		
+
 	// To get an user
 	@GetMapping("/users/{id}")
 	public ResponseEntity<User> getUserById(@PathVariable("id") long id) {
@@ -62,18 +62,19 @@ public class UserController {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 	}
-	
+
 	// To create a new user
 	@PostMapping("/users")
 	public ResponseEntity<User> createUser(@RequestBody User user) {
 		try {
-			User _user = userRepository.save(new User(user.getUsername(), user.getPassword(), user.getFullName(),user.getPhoneNumber()));
+			User _user = userRepository
+					.save(new User(user.getUsername(), user.getPassword(), user.getFullName(), user.getPhoneNumber()));
 			return new ResponseEntity<>(_user, HttpStatus.CREATED);
 		} catch (Exception e) {
 			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-	
+
 	// To update an user
 	@PutMapping("/users/{id}")
 	public ResponseEntity<User> updateUser(@PathVariable("id") long id, @RequestBody User user) {
@@ -87,7 +88,7 @@ public class UserController {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 	}
-	
+
 	// To delete an user
 	@DeleteMapping("/users/{id}")
 	public ResponseEntity<HttpStatus> deleteUser(@PathVariable("id") long id) {
@@ -110,4 +111,3 @@ public class UserController {
 		}
 	}
 }
-
